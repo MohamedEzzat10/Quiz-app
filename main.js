c3da3d06356b2ba3d5a29ff4a854aa974edf5cf7
MOHAMED EZZAT ELAHMADY <89709577+MohamedEzzat10@users.noreply.github.com>
2022-01-06 21:37:08 +0200

    Add files via upload
    new changes

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let questionTitleEle = document.querySelector(".question-title");
-let answersTitle = document.querySelectorAll("label")
-let answersEle = document.querySelectorAll(".answers input[name='answer']")
+var answersEle = document.querySelector(".answers ")
 let numberofquestionEle = document.querySelector(".numberofquestion");
 let btnNext = document.querySelector(".next")
 let  btnShowResult =document.querySelector(".show-result")
@@ -14,6 +13,12 @@ let infoName = document.querySelector(".persone-name")
 let popUPStart = document.querySelector(".informations")
 let popUpResult = document.querySelector(".pop-result")
 let inputName = document.querySelector(".name")
+let stroge ="";
+
+
+  var answersSpan = document.querySelectorAll(".answers span ")
+
+ 
 // satrt Quiz
 inputName.focus();
 startQuiz.addEventListener("click",()=>{
@@ -33,74 +38,106 @@ inputName.addEventListener("keyup",(e)=>{
   }
 })
 
+
+  btnNext.addEventListener("click",()=>{
+    if(stroge =="" ){
+      alert("please chose any answer")
+    } else{
+    if(questionCount <= 19){
+      storageAnswer()
+      getQuizFromApi()
+       
+      questionCount ++
+      countQuestion()
+    }    
+      if(questionCount == 19){
+       btnNext.style.display="none"
+       btnShowResult.style.display ="block"
+      
+  
+
+  }
+  
+    
+  }}) 
+  // show Result
+  btnShowResult.addEventListener("click",()=>{
+    if(stroge =="" ){
+      alert("please chose any answer")
+    } else{
+      storageAnswer()
+      countQuestion()
+
+      showResult()
+    }
+  })
+
+
+
+
+
 // get data from api
 getQuizFromApi()
 // set number question in element dom
-numberofquestionEle.innerHTML =numberQuestion
-
-btnNext.addEventListener("click",()=>{
-  if(numberQuestion <= 19){
-    stogeAnswer()
-
-    getQuizFromApi()
-   
-    questionCount ++
-    countQuestion()
-
-if(numberQuestion == 20){
-  btnNext.style.display ="none"
-  btnShowResult.style.display = "block"
-}
-}
-
-  
-})
+numberofquestionEle.innerHTML = numberQuestion
+// fetch  data from api 
 function getQuizFromApi(){ fetch('https://opentdb.com/api.php?amount=20&type=multiple')
   .then(response => response.json())
   .then(data =>{
 let questions = data.results  ;
 // all answer from api
 let allAnswer = questions[`${questionCount}`].incorrect_answers.concat(questions[`${questionCount}`].correct_answer);
-
 // swaping answer in array
 shuffle(allAnswer)
 correctanswers.push(questions[`${questionCount}`].correct_answer)
+console.log(questions[`${questionCount}`].correct_answer);
+// answersEle.innerHTML = eleAnswers
 questionTitleEle.innerHTML = questions[`${questionCount}`].question
-
-for(let i = 0 ;i< answersTitle.length  ;i++){
+// creatEle()
+// 
+for(let i = 0 ;i< 4  ;i++){
   // add answer to label inupt
-  answersTitle[i].innerHTML = allAnswer[i]
-  // add answer to iunpt value
-  answersEle[i].dataset.value = allAnswer[i]
-}
-});}
+  answersSpan[i].innerHTML =allAnswer[i]
 
+}
+answersSpan.forEach(e=>{
+  
+  e.classList.remove("active")
+})
+
+});}
 // function to counter number of question 
 function countQuestion(){
   if(numberQuestion < 20){
-
-    
     numberQuestion++
   } 
   numberofquestionEle.innerHTML = numberQuestion
 }
-// storge answer user
- function stogeAnswer(){
-  answersEle.forEach((e)=>{
-    
-     if(e.checked){
-     answersChecked.push(e.dataset.value)
-
-     
-   }
+//
+document.addEventListener("click",(e)=>{
+  if(e.target.classList == "answer"){
+    answersSpan.forEach(e=>{
   
+  e.classList.remove("active")
+})
+e.target.classList.add("active")
+stroge = e.target.innerHTML
+console.log(stroge);
+  }
 
+}) 
+
+
+
+function storageAnswer(){
+        if(correctanswers[questionCount ] == stroge){
+              result ++
+            }
+            stroge = "";
+            console.log(result);
+  }
  
-  })
-  if(answersChecked[questionCount -1] == correctanswers[questionCount-1]){
-        result ++
-      }
- }
+ 
 //  function shuffle array
 function shuffle(array) {
   let currentIndex = array.length,
@@ -120,26 +157,25 @@ function shuffle(array) {
 }
 
 
-// function showResult(){
-//   let nemeEle = document.querySelector(".head-result .name");
-//   // nemeEle.innerHTML = infoName.innerHTML
-// }
-// showResult()
 
 
 
 // show result 
-btnShowResult.addEventListener("click",()=>{
-  if(numberQuestion ===20){
 
-    stogeAnswer()
+
+  function showResult(){
+    storageAnswer()
+
     popUpResult.style.display ="block"
     document.querySelector(".head-result .name ").innerHTML =  infoName.innerHTML;
     document.querySelector(".degrea span").innerHTML = result;
    let parsntage = document.querySelector(".parsntage span");
    parsntage.innerHTML = Math.floor( eval(`${result}/${numberQuestion}*100`))
     let apappreciation= document.querySelector(".appreciation span");
-    if(parsntage.innerHTML < 65){
+     if(parsntage.innerHTML < 50){
+      apappreciation.innerHTML = "Fail the Quiz"
+    }
+    else if(parsntage.innerHTML < 65){
       apappreciation.innerHTML = "Acceptable" 
     }else if(parsntage.innerHTML < 75){
       apappreciation.innerHTML = "Good"
@@ -148,14 +184,14 @@ btnShowResult.addEventListener("click",()=>{
     }else if(parsntage.innerHTML <= 100){
       apappreciation.innerHTML = "Excellent"
     }
-  }
+  
   let newQuiz = document.querySelector(".new-quiz span")
   newQuiz.addEventListener("click",()=>{
     location.reload()
-
-  })
-})
+  })}
 
 
 
 
+
+  
